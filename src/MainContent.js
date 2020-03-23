bff045167889caae09624430394363c02d19feb9
vbenoit <vincent.benoit.site.web@gmail.com>
2020-03-23 21:13:48 +0100

    feat: animation line after line

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MainContent.css';
 import TypeWriterDiv from './TypeWriterDiv';
 
@@ -6,10 +6,39 @@ import {texts} from './Texts';
 
 export default function MainContent(props) {
 
-    console.log(texts[props.type][props.lang]);
+    const lines = texts[props.type][props.lang];
 
-    const content = 
-        texts[props.type][props.lang].map( (text) => <TypeWriterDiv key={text.key} text={text.value} /> );
+    const [content, setContent] = useState();
+    //const [lineIndex, setLineIndex] = useState(1);
+    const [pristine, setPristine] = useState(true);
+    const [previousType, setPreviousType] = useState("");
+
+    if ( props.type !== previousType ) {
+        setPristine(true);
+        setPreviousType(props.type);
+    }
+
+    function appendElements( lineIndex ) {
+
+        setPristine(false);
+
+        if ( lineIndex < lines.length ){
+            lineIndex ++;
+        }
+
+        setContent(
+            lines.slice(0,lineIndex).map( 
+                (text) => 
+                    <TypeWriterDiv 
+                        divClass="typewriter"
+                        lineIndex={lineIndex} appendElements={appendElements} 
+                        key={text.key} text={text.value} /> )
+        )
+    }
+
+    if (pristine) {
+        appendElements(0);
+    }
 
     return (
         content
--- src/TypeWriterDiv.js
+++ src/TypeWriterDiv.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TypeWriterDiv.css';
 
 export default function TypeWriterDiv(props) {
 
+    let classRemoved = false;
+    const [divClass, setDivClass] = useState( props.divClass );
+    
+    function removeClass(){
+        if (!classRemoved) {
+            setDivClass("");
+            classRemoved = true;
+        }
+        props.appendElements(props.lineIndex);
+    }
+
     return (
-        <div className="typewriter">
+        <div onAnimationEnd={removeClass} className={divClass} >
             <p>{props.text}</p>
         </div>
     );
